4703bc49817172579daeb5eea390bfd8dbd593f1
Angel G <agustavo79@gmail.com>
2017-03-06 14:42:00 -0600

    Comentarios en send.py y receive.py

--- receive.py
+++ receive.py
@@ -1,16 +1,25 @@
 import pika
 
+#Para consumir el mensaje, creamos la conexion
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 
 channel = connection.channel()
 
+# Este paso se podria omitir si estamos seguros de que existe la cola, pero aun asi
+# es buena practica validarlo con la misma linea de codigo con la cual se declara.
 channel.queue_declare(queue='hello')
 
+# suscribimos la funcion callback a la cola, cuando se reciba un mensaje esta funcion
+# es llamada por la libreria  pika
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
 
+# Luego le decimos a rabbit que esta funcion (callback) va a recibir los mensajes de la cola,
+# obvio para que tenga exito esta funcion la cola debe existir
 channel.basic_consume(callback,
                       queue='hello',
                       no_ack=True)
 print(' [*] Waiting for messages. To exit press CTRL+C')
+# Con esta instruccion entramos en un ciclo en espera de datos y de que se ejecute la funcion callback
+# cuando sea necesario
 channel.start_consuming()
--- send.py
+++ send.py
@@ -1,13 +1,26 @@
 import pika
 
+# Estas dos lineas crean la conexion con el servidor rabbitMQ, en este caso
+# el servidor esta instalado en esta maquina por eso apunta a localhost
+# en caso de que este en una pc remota hay que escribir la ip.
+
+
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 
 channel = connection.channel()
 
+# Se le da un nombre a la cola
+
 channel.queue_declare(queue='hello')
+
+# Se publica el mensaje especificando el nombre de la pila en el parametro routing_key
+# el mensaje en el parametro body y exchange se vera mas adelante.
 
 channel.basic_publish(exchange='',
                       routing_key='hello',
                       body='Hola World!')
 print(" [x] Sent 'Hello World!'")
+
+# Para liberar los buffers de red y asegurarnos de que el mensaje fue entregado simplemente
+# cerramos la conexion
 connection.close()
